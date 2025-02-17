961e6063d7bf65628f3cccf979578e59803f24b5
unknown <lovexctk@gmail.com>
2025-02-17 10:00:25 +0800

    fix: init data

--- projects/app/src/pages/api/common/system/getInitData.ts
+++ projects/app/src/pages/api/common/system/getInitData.ts
@@ -3,6 +3,7 @@ import { ApiRequestProps } from '@fastgpt/service/type/next';
 import { NextAPI } from '@/service/middleware/entry';
 import { InitDateResponse } from '@/global/common/api/systemRes';
 import { SystemModelItemType } from '@fastgpt/service/core/ai/type';
+import { authCert } from '@fastgpt/service/support/permission/auth/common';
 
 async function handler(
   req: ApiRequestProps<{}, { bufferId?: string }>,
@@ -24,99 +25,36 @@ async function handler(
     requestAuth: undefined
   })) as SystemModelItemType[];
 
-  // If bufferId is the same as the current bufferId, return directly
-  if (bufferId && global.systemInitBufferId && global.systemInitBufferId === bufferId) {
+  try {
+    await authCert({ req, authToken: true });
+    // If bufferId is the same as the current bufferId, return directly
+    if (bufferId && global.systemInitBufferId && global.systemInitBufferId === bufferId) {
+      return {
+        bufferId: global.systemInitBufferId,
+        systemVersion: global.systemVersion
+      };
+    }
+
     return {
       bufferId: global.systemInitBufferId,
-      systemVersion: global.systemVersion || '0.0.0'
-    }
-  });
-}
-
-const defaultFeConfigs: FastGPTFeConfigsType = {
-  show_emptyChat: true,
-  show_git: true,
-  docUrl: 'https://doc.fastgpt.in',
-  openAPIDocUrl: 'https://doc.fastgpt.in/docs/development/openapi',
-  systemTitle: 'YiLiao.AI',
-  concatMd: '关于我们: [YiLiao.AI](https://dataclubs.com/#about)',
-  limit: {
-    exportDatasetLimitMinutes: 0,
-    websiteSyncLimitMinuted: 0
-  },
-  scripts: [],
-  favicon: '/favicon.ico',
-  uploadFileMaxSize: 500
-};
-
-export async function getInitConfig() {
-  if (global.systemInitd) return;
-  global.systemInitd = true;
-
-  try {
-    await connectToDatabase();
-
-    await Promise.all([
-      initSystemConfig(),
-      // getSimpleModeTemplates(),
-      getSystemVersion(),
-      getSystemPlugin(),
-
-      // abandon
-      getSystemPluginV1()
-    ]);
-
-    console.log({
-      communityPlugins: global.communityPlugins
-    });
+      feConfigs: global.feConfigs,
+      subPlans: global.subPlans,
+      systemVersion: global.systemVersion,
+      activeModelList,
+      defaultModels: global.systemDefaultModel
+    };
   } catch (error) {
-    console.error('Load init config error', error);
-    global.systemInitd = false;
-
-    if (!global.feConfigs) {
-      exit(1);
+    const unAuthBufferId = global.systemInitBufferId ? `unAuth_${global.systemInitBufferId}` : '';
+    if (bufferId && unAuthBufferId === bufferId) {
+      return {
+        bufferId: unAuthBufferId
+      };
     }
+    return {
+      bufferId: unAuthBufferId,
+      feConfigs: global.feConfigs
+    };
   }
-}
-
-export async function initSystemConfig() {
-  // load config
-  const [dbConfig, fileConfig] = await Promise.all([
-    getFastGPTConfigFromDB(),
-    readConfigData('config.json')
-  ]);
-  const fileRes = JSON.parse(fileConfig) as FastGPTConfigFileType;
-
-  // get config from database
-  const config: FastGPTConfigFileType = {
-    feConfigs: {
-      ...fileRes?.feConfigs,
-      ...defaultFeConfigs,
-      ...(dbConfig.feConfigs || {}),
-      isPlus: !!FastGPTProUrl
-    },
-    systemEnv: {
-      ...fileRes.systemEnv,
-      ...(dbConfig.systemEnv || {})
-    },
-    subPlans: dbConfig.subPlans || fileRes.subPlans,
-    llmModels: dbConfig.llmModels || fileRes.llmModels || [],
-    vectorModels: dbConfig.vectorModels || fileRes.vectorModels || [],
-    reRankModels: dbConfig.reRankModels || fileRes.reRankModels || [],
-    audioSpeechModels: dbConfig.audioSpeechModels || fileRes.audioSpeechModels || [],
-    whisperModel: dbConfig.whisperModel || fileRes.whisperModel
-  };
-
-  // set config
-  initFastGPTConfig(config);
-
-  console.log({
-    feConfigs: global.feConfigs,
-    subPlans: global.subPlans,
-    systemVersion: global.systemVersion || '0.0.0',
-    activeModelList,
-    defaultModels: global.systemDefaultModel
-  };
 }
 
 export default NextAPI(handler);
